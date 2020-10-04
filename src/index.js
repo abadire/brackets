e309e3f86aebfa4cd419559ca6111703f06eac41
abadire <panvan97@gmail.com>
2020-10-04 17:16:45 +0300

    feat: implement check()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function check(str, bracketsConfig, mapped)
+{
+  if (str === '') return true;
+  if (str.length % 2 === 1) return false;
+
+  if (!mapped)
+  {
+    var mapped = {};
+    bracketsConfig.forEach(function (el)
+    {
+      mapped[el[0]] = el[1];
+    });
+  }
+
+  for (let i = 0; i < str.length; ++i)
+  {
+    if (!(str[i] in mapped)) return false;
+    let left = str[i];
+    let counter = 0; // Counts same inner brackets
+    for (let j = i + 1; j < str.length; ++j)
+    {
+      if (str[j] === left && left !== mapped[left]) counter++;
+      else if (str[j] === mapped[left] && counter > 0) counter--;
+      else if (str[j] === mapped[left] && counter === 0)
+      {
+        if (!check(str.slice(i + 1, j), bracketsConfig, mapped)) return false;
+        i = j;
+        break;
+      }
+    }
+  }
+
+  return true;
 }
+
+module.exports = check;
+// console.log(check(
+//   '|(|)', [['(', ')'],['|','|']]));
